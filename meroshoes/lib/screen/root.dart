93ec486a16064d7b790f75e64e64f3c34429d5bb
shweta kc <devshwetakc@hotmail.com>
2022-02-02 16:24:08 +0545

    add menu icon

--- meroshoes/lib/screen/root.dart
+++ meroshoes/lib/screen/root.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:meroshoes/color.dart';
 
 class Root extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: lightGrey,
+      appBar: getAppBar(),
+    );
+  }
+
+  Widget getAppBar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(55),
+      child: SafeArea(
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
+        child: Row(
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset("assets/images/menu.svg", height: 24))
+          ],
+        ),
+      )),
     );
   }
 }
